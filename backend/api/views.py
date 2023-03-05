967ccd0e1b798afb5ca5a77fc6facc04a9cffc19
Niki <NikSen09@mail.ru>
2023-03-05 22:28:09 +0400

    isort....

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
+
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
