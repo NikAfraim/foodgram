218b993f8c4bb7f9ca0d413a69c760ad1566e9af
Niki <NikSen09@mail.ru>
2023-03-05 22:19:57 +0400

    isort....

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,6 @@
 from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-
 from recipes.models import (Favourites, Ingredient, IngredientAmount, Recipe,
                             ShopList, Tag)
 from user.models import Subscription, User
