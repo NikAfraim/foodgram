7a770a3e33853e3b8f6e717b70dbae1505b24770
Niki <NikSen09@mail.ru>
2023-03-05 22:37:50 +0400

    isort....

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,14 +3,11 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
-
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
-from recipes.models import (Ingredient, IngredientAmount, Favourites, Recipe,
+from recipes.models import (Favourites, Ingredient, IngredientAmount, Recipe,
                             ShopList, Tag)
-
 from user.models import Subscription, User
 
 from .filters import IngredientFilter, RecipesFilter
